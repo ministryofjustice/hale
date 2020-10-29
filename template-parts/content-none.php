06a0ab24ef9721c95fe47e1e579d59e63cb411f8
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-10-29 11:57:45 +0000

    Remove additional search form, fix pagination styles

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -38,15 +38,11 @@
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'nightingale' ); ?></p>
 			<?php
-			get_search_form();
-
 		else :
 			?>
 
 			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'nightingale' ); ?></p>
 			<?php
-			get_search_form();
-
 		endif;
 		?>
 	</div><!-- .page-content -->
