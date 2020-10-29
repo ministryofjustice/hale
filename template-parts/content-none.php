f98f398b90eb99f1f1f9a584eeec26438527bd0c
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2020-10-29 14:03:54 +0000

    Theme name update

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -4,16 +4,16 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package Nightingale
- * @copyright NHS Leadership Academy, Tony Blacker
- * @version 1.1 21st August 2019
+ * @package Hale
+ * @copyright Ministry of Justice
+ * @version 1.0
  */
 
 ?>
 
 <section class="no-results not-found">
 	<header class="entry-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'nightingale' ); ?></h1>
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'hale' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
@@ -23,7 +23,7 @@
 			printf(
 				'<p>' . wp_kses(
 					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'nightingale' ),
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'hale' ),
 					array(
 						'a' => array(
 							'href' => array(),
@@ -36,12 +36,12 @@
 		elseif ( is_search() ) :
 			?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'nightingale' ); ?></p>
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'hale' ); ?></p>
 			<?php
 		else :
 			?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'nightingale' ); ?></p>
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'hale' ); ?></p>
 			<?php
 		endif;
 		?>
