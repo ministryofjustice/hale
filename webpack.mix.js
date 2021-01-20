3784d196def9c002e59c0f57d92a709b45101e49
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-01-20 09:18:56 +0000

    Move Sass files , rename npm dev to build

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,15 +3,13 @@ const mix_ = require('laravel-mix');
 
 mix_.setPublicPath('./dist')
   .copy('./assets/webfonts/*', 'dist/webfonts/')
-  .sass('./style.scss', 'css/style.min.css')
-  .sass('./style-gutenburg.scss', 'css/style-gutenburg.min.css')
-  .sass('./page-colours.scss', 'css/page-colours.min.css')
+  .sass('./assets/scss/style.scss', 'css/style.min.css')
+  .sass('./assets/scss/style-gutenburg.scss', 'css/style-gutenburg.min.css')
+  .sass('./assets/scss/page-colours.scss', 'css/page-colours.min.css')
   .copy('./assets/js/*', 'dist/js/')
   .options({
     processCssUrls: false
   });
-  //.copy('./assets/img/*', 'dist/images/');
- //.js('./assets/js/_main.js', 'js/main.min.js')
 
 if (mix_.inProduction()) {
   mix_.version();
