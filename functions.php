ae0942a80b8f31c3ee23f5a65a5bf78dac9e0e33
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-03-11 10:23:50 +0000

    Move to Seperate File

--- functions.php
+++ functions.php
@@ -258,46 +258,6 @@ function hale_scripts()
 
 add_action('wp_enqueue_scripts', 'hale_scripts');
 
-function hale_allowed_block_types( $allowed_blocks ) {
-
-    $restrict_blocks = get_theme_mod('restrict_blocks', "yes");
-
-    if($restrict_blocks == "yes") {
-
-        return array(
-            'core/image',
-            'core/paragraph',
-            'core/heading',
-            'core/list',
-            'core/code',
-            'core/file',
-            'core/video',
-            'core/columns',
-            'core/group',
-            'core/spacer',
-            'core/social-links',
-            'core/social-link',
-            'mojblocks/accordion',
-            'mojblocks/accordion-section',
-            'mojblocks/banner',
-            'mojblocks/card',
-            'mojblocks/cta',
-            'mojblocks/hero',
-            'mojblocks/highlights-list',
-            'mojblocks/intro',
-            'mojblocks/quote',
-            'mojblocks/reveal',
-            'mojblocks/staggered-box'
-        );
-    }
-    else {
-        return $allowed_blocks;
-    }
-
-}
-
-add_filter( 'allowed_block_types', 'hale_allowed_block_types' );
-
 /**
  * @param $filename
  * @return string
@@ -444,6 +404,11 @@ if (function_exists('gtm4wp_the_gtm_tag')) {
         require get_template_directory() . '/inc/google-tag-manager.php';
     }
 }
+
+/**
+ * Restrict Blocks
+ */
+require get_template_directory() . '/inc/restrict-blocks.php';
 
 /**
  * Taxonomies
