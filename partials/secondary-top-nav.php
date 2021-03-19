6c39e0b6f902e33246f0712e9c7e4111839a46c4
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2021-03-19 12:18:40 +0000

    Secondary Top Nav changes from Main - new files

---
+++ partials/secondary-top-nav.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Secondary Top Navigation Menu for our theme
+ *
+ */
+
+if (has_nav_menu('secondary-top-menu')) {
+    $menu_args = array(
+        'menu_id' => 'secondary-top-menu',
+        'container' => false,
+        'depth' => 1,
+        'theme_location' => 'secondary-top-menu'
+    );
+    ?>
+    <nav class="secondary-top-nav" id="secondary-top-navigation" role="navigation" aria-label="Seconday Top navigation"
+         aria-labelledby="label-navigation">
+        <div class="govuk-width-container">
+            <?php
+            wp_nav_menu($menu_args);
+            ?>
+        </div>
+    </nav>
+<?php }
