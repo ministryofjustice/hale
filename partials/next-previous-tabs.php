d139a1a91bda7c3c4fd8f7fd506face68dae79da
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2021-03-30 12:41:17 +0100

    artical navigation styling

--- partials/next-previous-tabs.php
+++ partials/next-previous-tabs.php
@@ -28,7 +28,7 @@ if ($is_cat_page && (!empty($prev_page) || !empty($next_page))) { ?>
         if (!empty($prev_page)) {
             ?>
             <li class="gem-c-pagination__item gem-c-pagination__item--previous">
-              <a href="<?php echo get_permalink($prev_page->ID); ?>" class="gem-c-pagination__link" rel="prev">
+              <a href="<?php echo get_permalink($prev_page->ID); ?>" class="gem-c-pagination__link hale-article-nav hale-article-nav--previous" rel="prev">
                 <span class="gem-c-pagination__link-title">
                   <svg class="gem-c-pagination__link-icon" xmlns="http://www.w3.org/2000/svg" height="13" width="17" viewBox="0 0 17 13">
                     <path d="m6.5938-0.0078125-6.7266 6.7266 6.7441 6.4062 1.377-1.449-4.1856-3.9768h12.896v-2h-12.984l4.2931-4.293-1.414-1.414z"></path>
@@ -49,7 +49,7 @@ if ($is_cat_page && (!empty($prev_page) || !empty($next_page))) { ?>
         if (!empty($next_page)) {
             ?>
             <li class="gem-c-pagination__item gem-c-pagination__item--next">
-              <a href="<?php echo get_permalink($next_page->ID); ?>" class="gem-c-pagination__link" rel="next">
+              <a href="<?php echo get_permalink($next_page->ID); ?>" class="gem-c-pagination__link hale-article-nav hale-article-nav--next" rel="next">
                 <span class="gem-c-pagination__link-title">
                   <svg class="gem-c-pagination__link-icon" xmlns="http://www.w3.org/2000/svg" height="13" width="17" viewBox="0 0 17 13">
                     <path d="m10.107-0.0078125-1.4136 1.414 4.2926 4.293h-12.986v2h12.896l-4.1855 3.9766 1.377 1.4492 6.7441-6.4062-6.7246-6.7266z"></path>
