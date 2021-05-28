0d1fd807fdba6cf70a6c392df15476d7716290f4
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-05-28 16:02:32 +0100

    Comment update

--- page-full-width.php
+++ page-full-width.php
@@ -3,13 +3,6 @@
 /**
  * Template Name: Full-Width
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package Hale
  * @copyright Ministry of Justice
  * @version 2.0
