db53f1d884ff1bcc3927ff055f37a38f0b8d7448
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-07-08 11:08:19 +0100

    Allow twitter block

--- assets/js/restrict-embed-blocks.js
+++ assets/js/restrict-embed-blocks.js
@@ -1,6 +1,47 @@
+/*
+Restricts Embed Blocks
+
+List of embed blocks
+---
+
+amazon-kindle
+animoto
+cloudup
+collegehumor
+crowdsignal
+dailymotion
+facebook
+flickr
+imgur
+instagram
+issuu
+kickstarter
+meetup-com
+mixcloud
+reddit
+reverbnation
+screencast
+scribd
+slideshare
+smugmug
+soundcloud
+speaker-deck
+spotify
+ted
+tiktok
+tumblr
+twitter
+videopress
+vimeo
+wordpress
+wordpress-tv
+youtube
+ */
+
 wp.domReady(function () {
   const allowedEmbedBlocks = [
     'youtube',
+    'twitter'
   ];
   wp.blocks.getBlockVariations('core/embed').forEach(function (blockVariation) {
     if (-1 === allowedEmbedBlocks.indexOf(blockVariation.name)) {
