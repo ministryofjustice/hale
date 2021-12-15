2bba03ce03a06e4b991eae4169a508a78e0c403b
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2021-12-15 11:07:17 +0000

    Adjust Posted on function to only show published date

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -15,23 +15,9 @@ if ( ! function_exists( 'hale_posted_on' ) ) :
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function hale_posted_on() {
-		$date_enabled = get_theme_mod( 'blog_date_display', 'true' );
-		if ( 'true' === $date_enabled ) {
-			$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-			if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-				$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-			}
 
-			$time_string = sprintf(
-				$time_string,
-				esc_attr( get_the_date( DATE_W3C ) ),
-				esc_html( get_the_date() ),
-				esc_attr( get_the_modified_date( DATE_W3C ) ),
-				esc_html( get_the_modified_date() )
-			);
+        echo '<time class="entry-date published-date" datetime="' . get_the_date( DATE_W3C ) . '">' . get_the_date() . '</time>';
 
-			echo '<span class="govuk-visually-hidden">Posted on: </span>' . $time_string . ' '; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		}
 	}
 endif;
 
