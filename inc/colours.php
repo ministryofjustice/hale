188eea84e8a30c3886bbb01477e3b2a63fca596c
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2022-06-30 09:34:41 +0100

    rationalise

--- inc/colours.php
+++ inc/colours.php
@@ -32,8 +32,8 @@
 			//header
 			['header-bg',$black,'Header background','',''],
 			['header-link',$white,'Header link','',''],
-			['header-link-current',$currentPageBlue,'Header current page','',''],
-			['header-link-current-bg',$black,'Header current page background','Desktop only, narrow displays extend the main colour ??? TBC',''],
+			['header-link-current',$currentPageBlue,'Header current page','Main menu and submenu',''],
+			['header-link-current-bg',$black,'Header current page background','Main menu and submenu',''],
 			['header-link-hover-border',$white,'Header link hover underline','',''],
 			['header-link-focus',$black,'Header link focus','',''],
 			['header-link-focus-highlight',$yellow,'Header link focus highlight','',''],
@@ -55,12 +55,10 @@
 			['header-submenu-arrow',$white,'Submenu arrow','',''],
 			['header-submenu-arrow-focus',$black,'Submenu arrow focus','',''],
 			['header-submenu-bg',$lightGrey,'Submenu background','Desktop only, narrow displays extend the main colour',''],
-			['header-submenu-link-current-bg',$lightGrey,'Submenu link current page background','',''],
 			['header-submenu-top-border',$headerMenuLineGrey,'Submenu dividing line','',''],
 			['header-submenu-link-mobile',$white,'Submenu link (mobile)','',''],
 			['header-submenu-link-mobile-current',$white,'Submenu link (mobile) current page','',''],
 			['header-submenu-link',$black,'Submenu link (desktop)','',''],
-			['header-submenu-link-current',$black,'Submenu link (desktop) current page','',''],
 			['header-submenu-link-hover-underline',$black,'Submenu link hover underline','',''],
 
 			//Misc
