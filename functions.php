3a2f856bbd22e42d74949a3625f4a8de82962a1a
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2022-07-18 11:48:36 +0100

    Added JS for accordion to apply unique IDs and ID auto-expand when direct linking

--- functions.php
+++ functions.php
@@ -254,6 +254,7 @@ function hale_scripts() {
 
     // JS
     wp_enqueue_script('govuk-frontend', hale_mix_asset('/js/govuk-frontend.js'), '', "4.2.0", true);
+    wp_enqueue_script('hale-accordion-auto-expand', hale_mix_asset('/js/accordion-auto-expand.js'), '', null, true);
     wp_enqueue_script('hale-scripts', hale_mix_asset('/js/hale-scripts.js'), '', null, true);
     wp_enqueue_script('hale-skip-link-focus-fix', hale_mix_asset('/js/skip-link-focus-fix.js'), '', null, true);
     wp_enqueue_script('hale-navigation', hale_mix_asset('/js/navigation.js'), '', null, true);
