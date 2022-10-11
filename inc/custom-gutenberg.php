36b67da7e723177aca25844ee839fa2a4b320516
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-10-11 14:53:02 +0100

    Only include custom styles on edit page - fixes issue with customizer

--- inc/custom-gutenberg.php
+++ inc/custom-gutenberg.php
@@ -14,24 +14,28 @@
  */
 function hale_gutenberg_editor_styles() {
 
-    wp_enqueue_style('hale-gutenburg-style', hale_mix_asset('/css/style-gutenburg.min.css'));
+    $screen = get_current_screen();
+    //Only apply to edit backend pages
+    if ($screen->base == "post") {
+        wp_enqueue_style('hale-gutenburg-style', hale_mix_asset('/css/style-gutenburg.min.css'));
 
-    $browser_is_IE = (isset($_SERVER['HTTP_USER_AGENT']) && ( (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false ) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false) ) );
+        $browser_is_IE = (isset($_SERVER['HTTP_USER_AGENT']) && ((strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) || (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident/7.0; rv:11.0') !== false)));
 
-    if (!$browser_is_IE) wp_enqueue_style('hale-custom-branding', hale_mix_asset('/css/custom-branding.min.css'));
+        if (!$browser_is_IE) wp_enqueue_style('hale-custom-branding', hale_mix_asset('/css/custom-branding.min.css'));
 
-    $t=time();
-    $css_file_name = "/custom-colours.css?t=$t";
-    $css_file_name_IE = "/custom-colours-ie.css?t=$t";
+        $t = time();
+        $css_file_name = "/custom-colours.css?t=$t";
+        $css_file_name_IE = "/custom-colours-ie.css?t=$t";
 
-    if (is_ssl()) {
-        //wp_get_upload_dir()["baseurl"] only returns http.
-        $baseURL = str_replace('http://','https://',wp_get_upload_dir()["baseurl"]);
-        if (!$browser_is_IE) wp_enqueue_style('hale-custom-colours', $baseURL . $css_file_name);
-        wp_enqueue_style('hale-custom-colours-ie', $baseURL . $css_file_name_IE);
-    } else {
-        if (!$browser_is_IE) wp_enqueue_style('hale-custom-colours', wp_get_upload_dir()["baseurl"] . $css_file_name);
-        wp_enqueue_style('hale-custom-colours-ie', wp_get_upload_dir()["baseurl"] . $css_file_name_IE);
+        if (is_ssl()) {
+            //wp_get_upload_dir()["baseurl"] only returns http.
+            $baseURL = str_replace('http://', 'https://', wp_get_upload_dir()["baseurl"]);
+            if (!$browser_is_IE) wp_enqueue_style('hale-custom-colours', $baseURL . $css_file_name);
+            wp_enqueue_style('hale-custom-colours-ie', $baseURL . $css_file_name_IE);
+        } else {
+            if (!$browser_is_IE) wp_enqueue_style('hale-custom-colours', wp_get_upload_dir()["baseurl"] . $css_file_name);
+            wp_enqueue_style('hale-custom-colours-ie', wp_get_upload_dir()["baseurl"] . $css_file_name_IE);
+        }
     }
 }
 
