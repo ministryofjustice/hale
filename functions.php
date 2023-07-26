d2271f2713fd76cf7f9ff14ab0390b6c7368a625
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-07-26 12:13:21 +0100

    Made more editor-friendly, hid until revealed

--- functions.php
+++ functions.php
@@ -553,17 +553,32 @@ function lang_attribute( $item_id, $item ) {
 	$lang_attribute = get_post_meta( $item_id, '_lang_attribute', true );
     ?>
     <div style="clear: both;">
-        <span class="description"><?php _e( "Language attribute "); ?></span><br />
-        <span class="description"><?php _e( "(only for different language links in the footer)"); ?></span><br />
-        <input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
-        <div class="logged-input-holder">
-            <input
-                type="text"
-                name="lang_attribute[<?php echo $item_id ;?>]"
-                id="lang-attr-<?php echo $item_id ;?>"
-                value="<?php echo esc_attr( $lang_attribute ); ?>"
-            />
-	    </div>
+        <details style="margin-bottom:1em;">
+            <summary class="button-link">
+                Link in different language?
+            </summary>
+            <input type="hidden" class="nav-menu-id" value="<?php echo $item_id ;?>" />
+            <label for="lang-attr-<?php echo $item_id ;?>">
+                <span class="description"><?php _e( "Language ISO code "); ?></span><br />
+                <input
+                    type="text"
+                    name="lang_attribute[<?php echo $item_id ;?>]"
+                    id="lang-attr-<?php echo $item_id ;?>"
+                    value="<?php echo esc_attr( $lang_attribute ); ?>"
+                /><br />
+                <span class="description" style="color:#b10e5e"><?php _e( "Only use if link text is a different language from the main page.  <br />Only works for links in the footer."); ?></span><br />
+            </label>
+            <div class="description">
+                Examples:
+                <ul style="list-style-type: square; margin-left: 2em;">
+                    <li><strong>cy</strong>: Welsh (general)</li>
+                    <li><strong>fr</strong>: French (general)</li>
+                    <li><strong>fr-CA</strong>: French (Québec)</li>
+                    <li><strong>es-ES</strong>: Spanish (Spain)</li>
+                    <li><strong>es-419</strong>: Spanish (Latin America)</li>
+                </ul>
+            </div>
+        </details>
 	</div>
 	<?php
 }
