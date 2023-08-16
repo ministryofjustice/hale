4cb1fda9b3e4f75b7535d613370ae9ed2ad43c95
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-08-16 12:04:56 +0100

    Added new items to arrays, new special case array for awkward words & ordinal handling

--- template-parts/address-management.php
+++ template-parts/address-management.php
@@ -9,7 +9,8 @@
 	 **/
 
 	function sanitizeAddress($address) {
-		$allCaps = ["HM","HMP","YOI","HMP/YOI","HMYOI","HMPYOI","NPS","NMS","RCJ","OPG","JCP","LAA","HMCTS","HMPPS","CTS"];
+		$allCaps = ["HM","HMP","YOI","HMP/YOI","HMYOI","HMPYOI","NPS","NMS","RCJ","OPG","JCP","LAA","HMCTS","HMPPS","CTS","LG"];
+		$specialCases = ["&amp;", "MoJ", "(MoJ)", "MacMillan"]; //instances where the capitalization isn't quite normal
 		$commonSeparators = [
 			// These are separators which are not capitalised at all
 			"on", "on-the",
@@ -20,8 +21,8 @@
 			"in", "in-the",
 			"to", "to-the",
 			"super",
-			"cum",
-			"with", "of",
+			"cum", "and",
+			"with", "of", "of-the",
 			"en-le", "de-la", "en-la",
 			"en", "de", "le", "la"];
 		$postcodePattern = '/^([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([AZa-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z])))) [0-9][A-Za-z]{2})$/';
@@ -43,7 +44,19 @@
 		$addressBits = explode(" ",$addressWithoutPostcode);
 		$sanitizedAddress = "";
 		foreach($addressBits as $bit) {
-			if (!in_array($bit,$allCaps) && !preg_match("/[0-9]/",$bit)) {
+			if (in_array(strtolower($bit), array_map('strtolower', $specialCases))) {
+				// this ensures that special cases are capitalized correctly
+				foreach($specialCases as $specialCase) {
+					if (strtolower($bit) == strtolower($specialCase)) $sanitizedAddress .= $specialCase." ";
+				}
+			} elseif (preg_match("/[0-9][SNRTsnrt][TDHtdh]/",$bit)) {
+				// this ensures items like 1st/2nd are lower case
+				$sanitizedAddress .= strtolower($bit)." ";
+			} elseif (in_array($bit,$allCaps)) {
+				// this ensures all-caps items are capitalized
+				$sanitizedAddress .= strtoupper($bit)." ";
+			} elseif (!preg_match("/[0-9]/",$bit)) {
+				// this is everything else if no numbers found in string
 				$bit = mb_strtolower($bit);
 				if (!in_array($bit,$commonSeparators)) {
 					$bit = mb_convert_case($bit, MB_CASE_TITLE)." ";
@@ -66,10 +79,8 @@
 					$separator = str_replace("-"," ",$separator);
 					$sanitizedAddress = str_ireplace(" $separator "," $separator ",$sanitizedAddress); //spaces
 				}
-
-					
 			} else  {
-				$sanitizedAddress .= strtoupper($bit)." ";
+				$sanitizedAddress .= $bit." ";
 			}
 		}
 		$sanitizedAddress .= $postcode;
