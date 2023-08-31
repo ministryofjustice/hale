26037b5a0466f15177a1f31d806f4b810fe20a34
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-08-31 11:15:26 +0100

    _e notation for docs

--- page-document-listing.php
+++ page-document-listing.php
@@ -88,10 +88,12 @@ while (have_posts()) :
                 <div class="document-listing-search-section">
                     <div class="document-listing-search-form">
                         <form action="<?php echo get_permalink(); ?>" method="GET">
-                            <label for="doc-search-field" class="govuk-visually-hidden">Search</label>
+                            <label for="doc-search-field" class="govuk-visually-hidden">
+                                <?php _e('Search', 'hale'); ?>
+                            </label>
                             <input class="govuk-input" id="doc-search-field" name="doc_search"
                                    value="<?php echo $search_text_HTML; ?>" type="search"
-                                   placeholder="Search">
+                                   placeholder="<?php _e('Search', 'hale'); ?>">
 
                             <?php
 
@@ -150,7 +152,9 @@ while (have_posts()) :
 
                                 if (!empty($dropdown_html)) { ?>
 
-                                    <label class="govuk-label" for="document-search-filter-type">Type</label>
+                                    <label class="govuk-label" for="document-search-filter-type">
+                                        <?php _e('Type', 'hale'); ?>
+                                    </label>
                                     <?php echo $dropdown_html; ?>
 
                                     <?php
@@ -178,7 +182,9 @@ while (have_posts()) :
 
                                 if (!empty($dropdown_html)) { ?>
 
-                                    <label class="govuk-label" for="document-search-filter-category">Category</label>
+                                    <label class="govuk-label" for="document-search-filter-category">
+                                        <?php _e('Category', 'hale'); ?>
+                                    </label>
                                     <?php echo $dropdown_html; ?>
 
                                     <?php
@@ -210,7 +216,9 @@ while (have_posts()) :
 
                                 if (!empty($dropdown_html)) { ?>
 
-                                    <label class="govuk-label" for="document-search-filter-location">Location</label>
+                                    <label class="govuk-label" for="document-search-filter-location">
+                                        <?php _e('Location', 'hale'); ?>
+                                    </label>
                                     <?php echo $dropdown_html; ?>
 
                                     <?php
@@ -219,7 +227,9 @@ while (have_posts()) :
                             }
                             ?>
 
-                            <button class="govuk-button">Search</button>
+                            <button class="govuk-button">
+                                <?php _e('Search', 'hale'); ?>
+                            </button>
                         </form>
 
                     </div>
