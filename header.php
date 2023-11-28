b2bd1f36f9ca10bbb60b1bcb09d901416c13e677
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2023-11-28 11:45:35 +0000

    Added suffix for links which open in a new tab

--- header.php
+++ header.php
@@ -15,11 +15,18 @@
 	} else {
 		$style_class = "hale-colours-variable";
 	}
+
+	if (!isset($opens_in_a_new_tab)) $opens_in_a_new_tab = "(opens in a new tab)";
 ?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="hale-page <?php printf($style_class); ?>">
 <head>
 
+	<style>
+		a[target=_blank]:after {
+			content: "<?php echo $opens_in_a_new_tab; ?>";
+		}
+	</style>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="theme" content="MOJ-hale-1.0.11">
