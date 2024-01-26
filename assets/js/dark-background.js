c28d084ea13a9652378da119011a3166e362f975
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-01-26 16:53:03 +0000

    Added CSS to switch links to dark style and JS to detect dark style

---
+++ assets/js/dark-background.js
@@ -0,0 +1,16 @@
+( function() {
+	markDarkBackground();
+})();
+
+function markDarkBackground(){
+	let customBackgroundElements = document.querySelectorAll(".has-generic-palette-1-background-color,.has-generic-palette-2-background-color,.has-generic-palette-3-background-color,.has-generic-palette-4-background-color"); // gets all the elements with a set background
+	for (let i = 0; i<customBackgroundElements.length; i++) {
+		let colourValue = getComputedStyle(customBackgroundElements[i]).backgroundColor.replace("rgb(", "").replace(")", "");; // should return an RGB value
+		let array = colourValue.split(", ");
+		if (array.length != 3) continue; // if for whatever reason we don't have an array length of 3 then we don't have an RGB value
+		luma = 0.2126 * array[0] + 0.7152 * array[1] + 0.0722 * array[2]; // per ITU-R BT.709 apparently - I just found this online
+		if (luma < 128) {
+			customBackgroundElements[i].classList.add("dark-background");
+		}
+	}
+}
