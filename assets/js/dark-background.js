6940eb438c6bc8c4b24272df89a499b7d6eb1e4e
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-01-29 13:29:07 +0000

    Amended class name and corrected heading css

--- assets/js/dark-background.js
+++ assets/js/dark-background.js
@@ -10,9 +10,9 @@ function markDarkBackground(){
 		if (array.length != 3) continue; // if for whatever reason we don't have an array length of 3 then we don't have an RGB value
 		luma = 0.2126 * array[0] + 0.7152 * array[1] + 0.0722 * array[2]; // per ITU-R BT.709 apparently - I just found this online
 		if (luma < 128) {
-			customBackgroundElements[i].classList.add("dark-background");
+			customBackgroundElements[i].classList.add("hale-dark-background");
 		} else {
-			customBackgroundElements[i].classList.add("light-background");
+			customBackgroundElements[i].classList.add("hale-light-background");
 		}
 	}
 
