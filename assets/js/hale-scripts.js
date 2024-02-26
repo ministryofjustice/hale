3e07122df83c0673cd857a636c6e0ad0bab0992b
Rob Lowe <robert.lowe@digital.justice.gov.uk>
2024-02-26 19:49:57 +0000

    Quick Exit Button - JS Method

--- assets/js/hale-scripts.js
+++ assets/js/hale-scripts.js
@@ -14,3 +14,80 @@ if( document.getElementById("search-show-hide") != null) {
     }
   );
 }
+
+jQuery( document ).ready(function( $ ) {
+
+  function fleeFromPage(fleeMethod) {
+    $("body").hide();
+    window.open("http://bbc.co.uk/weather", "_newtab");
+    window.location.replace("http://www.google.co.uk");   
+  }
+  $("a.govuk-exit-this-page__button").on("click", function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    fleeFromPage("mouse click");
+  });
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) { // 27 = escape key
+      fleeFromPage("escape key");
+    }
+  });
+
+  /*
+  window.addEventListener('scroll', function() {
+    var stickyDiv = document.getElementById('.govuk-exit-this-page');
+    var stickyDivRect = stickyDiv.getBoundingClientRect();
+  
+    if (window.scrollY >= stickyDivRect.top) {
+      stickyDiv.classList.add('sticky');
+    } else {
+      stickyDiv.classList.remove('sticky');
+    }
+  });*/
+
+  $(window).on('scroll resize', function() {
+    var stickyDiv = $('.govuk-exit-this-page');
+    var stickyButton = $('.govuk-exit-this-page a');
+
+    if (stickyDiv.length) {
+
+      var divMinHeight = stickyButton.outerHeight();
+      var divMinWidth = stickyButton.outerWidth();
+
+      if ($(".hale-header__mobile-controls--search").is(":visible")) {
+        //divMinHeight = 'initial';
+        divMinWidth = 0
+      }
+
+      stickyDiv.css({
+        "min-height": divMinHeight,
+        "min-width": divMinWidth
+       });
+
+      var stickyDivTop = stickyDiv.offset().top;
+      var stickyDivLeft = stickyDiv.offset().left;
+
+      if ($(window).scrollTop() >= stickyDivTop) {
+        stickyDiv.addClass('sticky');
+
+        $('.govuk-exit-this-page a').css({
+            "left": stickyDivLeft
+        });
+
+      } else {
+        stickyDiv.removeClass('sticky');
+      
+        $('.govuk-exit-this-page a').css({
+            "left": 'initial'
+        });
+      }
+
+      if ($(".hale-header__mobile-controls--search").is(":visible")) {
+        $('.govuk-exit-this-page a').css({
+          "left": 0
+        });
+      }
+    }
+  });
+
+});
