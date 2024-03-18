de0b6949659847c0092cf64d3e2f067ee0ad93e7
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-03-18 14:46:29 +0000

    Moved banner settings to one page with tabs

--- inc/banner-settings.php
+++ inc/banner-settings.php
@@ -1,19 +1,14 @@
 <?php
 if( function_exists('acf_add_options_page') ) {
-    $customizations = acf_add_options_page(array(
-        'page_title' 	=> 'Site Customization',
-        'menu_title'	=> 'Site Customization',
-        'menu_slug' 	=> 'site-customization',
-        'capability'    => 'edit_posts'
-    ));
-    $critical_information_banner_page = acf_add_options_sub_page(array(
-        'page_title' 	=> 'Critical Information Banner Settings',
-        'menu_title'	=> 'Critical Information Banner Settings',
-        'parent_slug'   => $customizations['menu_slug'],
-    ));
-    $emergency_banner_page = acf_add_options_sub_page(array(
-        'page_title' 	=> 'Emergency Banner Settings',
-        'menu_title'	=> 'Emergency Banner Settings',
-        'parent_slug'   => $customizations['menu_slug'],
-    ));
+	$customizations = acf_add_options_page(array(
+		'page_title'  => 'Customization',
+		'menu_title'  => 'Customization',
+		'menu_slug'   => 'site-customization',
+		'capability'  => 'edit_theme_options'
+	));
+	$banners = acf_add_options_sub_page(array(
+		'page_title'  => 'Site-wide Banner Settings',
+		'menu_title'  => 'Site-wide Banner Settings',
+		'parent_slug' => $customizations['menu_slug'],
+	));
 }
