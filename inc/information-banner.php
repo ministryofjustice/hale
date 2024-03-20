ebb34a13d80f479c4d26b8bb0b2b6a4bf52836e3
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-03-20 12:33:29 +0000

    Adjusted colours, info layout, ACF field text and added translations

--- inc/information-banner.php
+++ inc/information-banner.php
@@ -9,7 +9,7 @@ if( function_exists('get_field') ) {
         $banner_summary = get_field('info_banner_summary', 'option');
         $moreinfolink = get_field('info_banner_more_info', 'option');
         if (isset($moreinfolink) && trim($moreinfolink) != "") {
-            $banner_info_link = "<a class='govuk-link info-banner__link' href='$moreinfolink'>".__("More information","hale")."</a>";
+            $banner_info_link = "<a class='govuk-link info-banner__link' href='$moreinfolink'>".__("Find out more","hale")."</a>";
         } else {
             $banner_info_link = "";
         }
@@ -28,7 +28,9 @@ if( function_exists('get_field') ) {
                         ";
                     ?>
                 </div>
-                <a id='critical-info-banner-close-button' class='govuk-link info-banner__link info-banner__close' href='#'>Hide banner</a>
+                <a id='critical-info-banner-close-button' class='govuk-link info-banner__close' href='#'>
+                    <?php _e("Hide","hale"); ?>
+                </a>
             </div>
         </div>
         <?php
