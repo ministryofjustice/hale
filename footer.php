e6c3d06c6f76be45eba5b8b4cec1bcb70191542f
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2024-10-16 13:44:37 +0100

    Added SVG footer crest and ability to change the colours if needed

--- footer.php
+++ footer.php
@@ -57,15 +57,23 @@ flush();
       </div>
       <?php
         if ('yes' == $crown_copyright) {
+
+          $copyright_style = "padding-top:0; background-image:none;";
       ?>
         <div class="govuk-footer__meta-item">
           <a
             class="govuk-footer__link govuk-footer__copyright-logo"
             href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"
-            style="background-image:url(<?php echo get_template_directory_uri(); ?>/assets/images/govuk-crest-2x.webp);"
+            style="<?php echo $copyright_style; ?>"
           >
+          <svg xmlns="http://www.w3.org/2000/svg" class="govuk-footer__gov-crest" id="footer-crest" viewBox="0 0 702.47 624.08">
+            <?php
+              include 'inc/govuk-crest-svg-content.php';
+            ?>
+            <image src="<?php echo get_template_directory_uri(); ?>/assets/images/govuk-crest-2x.webp" xlink:href="" class="govuk-footer__crest-fallback-image" width="115" height="94"></image>
+          </svg>
           <?php
-              echo esc_html__('&copy; Crown copyright', 'hale');
+            echo esc_html__('&copy; Crown copyright', 'hale');
           ?>
           </a>
         </div>
