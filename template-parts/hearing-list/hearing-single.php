e988e46de0089bd99bf908137a2f2cdcdc0470f0
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-01-09 12:00:43 +0000

    Added Taxonomy on single view check

--- template-parts/hearing-list/hearing-single.php
+++ template-parts/hearing-list/hearing-single.php
@@ -6,16 +6,17 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(array('flexible-post-type-single')); ?>>
-    <header class="flexible-post-type-header">
+    <div class="flexible-post-type-header">
         <?php
 
         the_title('<h1 class="flexible-post-type-title govuk-heading-xl">', '</h1>'); ?>
-    </header>
+    </div>
     <?php
-     $show_published_date_on_single_view = hale_get_acf_field_status('show_published_date_on_single_view');
+    $show_published_date_on_single_view = hale_get_acf_field_status('show_published_date_on_single_view');
+    $show_tax_on_single_view = hale_get_acf_field_status('show_tax_on_single_view');
 
-     if($show_published_date_on_single_view) {
-     ?>
+    if($show_published_date_on_single_view) {
+    ?>
         <div class="flexible-post-type-published-date-wrapper">
             <div class="flexible-post-type-published-date">
                     <div class="flexible-post-type-published-date-label">Hearing date: </div>
@@ -29,7 +30,7 @@
         'hearing-type'
     );
     
-    if(!empty($display_terms_taxonomies)){
+    if($show_tax_on_single_view && !empty($display_terms_taxonomies)){
 
         $tax_details = hale_get_post_tax_details($display_terms_taxonomies);
         
