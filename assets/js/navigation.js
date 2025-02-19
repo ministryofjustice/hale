13d53e013ca02c2bd87464754025579d5e4cced9
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2025-02-19 17:56:13 +0000

    Added JS for scrolling current location, print button and styles

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -309,3 +309,47 @@ jQuery("#menu-menu-top-menu li.menu-item-has-children > ul.sub-menu").ready(func
 		if (e.keyCode == "13") $(this).click();
 	});
 });
+
+/**
+ *
+ * Table of contents "scrollspy", highlight current scroll location
+ *
+ */
+
+( function() {
+	indicateCurrentLocation();
+})();
+
+document.addEventListener('scroll', function() {
+	indicateCurrentLocation();
+}, false);
+
+function indicateCurrentLocation(){
+	if (!document.querySelector("#toc")) return;
+	let toc = document.querySelector("#table-of-contents");
+	let sectionHeadings = document.querySelectorAll(".hale-toc-item");
+	let contents = toc.querySelectorAll("li");
+	for (i=0; i+1<sectionHeadings.length; i++) {
+		let nextPosition = sectionHeadings[i+1].getBoundingClientRect().top;
+		if (nextPosition > 150) break; //we stop counting when the next one is above 150 as we are on the current item
+	}
+
+	// A small bit of code to ensure the last item is always "current" when at the very bottom of the page.
+	const documentHeight = Math.max(
+		document.body.scrollHeight,
+		document.body.offsetHeight,
+		document.documentElement.clientHeight,
+		document.documentElement.scrollHeight,
+		document.documentElement.offsetHeight
+	);
+	const windowHeight = window.innerHeight;
+	const maxScroll = documentHeight - windowHeight;
+
+	if (window.scrollY >= maxScroll) i = sectionHeadings.length - 1;
+
+	// We set the "current" class as per the current location
+	contents.forEach(item => {
+		item.classList.remove("hale-table-of-contents__item--current");
+	});
+	contents[i].classList.add("hale-table-of-contents__item--current");
+}
