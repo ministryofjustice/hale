82c9dba9ef16bbf842b2d9d4442170d5ca12af91
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2026-07-23 14:37:46 +0100

    Made the admin enqueue more page specific

--- functions.php
+++ functions.php
@@ -183,12 +183,22 @@ add_action('after_setup_theme', 'hale_content_width', 0);
  * Enqueue scripts and styles.
  */
 
-add_action( 'admin_enqueue_scripts', function() {
+add_action('edit_post_type_enqueue_scripts', function ($screen) {
+
+    if (!$screen) {
+        return;
+    }
+
+    // Check we are on a post edit screen
+    if ($screen->base !== 'post') {
+        return;
+    }
+
     wp_enqueue_style(
         'hale-admin',
         hale_mix_asset('/css/admin.min.css')
     );
-} );
+});
 
 function hale_action_customize_save_after($array)
 {
